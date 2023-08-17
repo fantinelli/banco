3352f98f60ace4fe4bea3ed9b2d20769aa9a322b
julia-015 <136704485+julia-015@users.noreply.github.com>
2023-08-17 07:52:02 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 class Cliente:
     # criação do cliente contendo uas informações
-    def __init__(self, nome, CPF, RG, telefone, email, renda, depinicial):
+    def __init__(self, nome, CPF, RG, telefone, email, renda, depinicial, saldo=0):
         self.nome = nome
         self.CPF = CPF
         self.RG = RG
@@ -8,6 +8,7 @@ class Cliente:
         self.email = email
         self.renda = renda
         self.depinicial = depinicial
+        self.saldo = saldo
 
     # retorna todas as informações do cliente
     def getCliente(self):
@@ -42,26 +43,8 @@ class Cliente:
         self.depinicial = x
 
 
-class Agencia:
-    # cria a lista de cliente da agencia bancaria
-    def __init__(self):
-        self._cliente = []
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    # adiciona um novo cliente a lista criada
-    def novo_cliente(self, cliente):
-        self._cliente.append(cliente) 
-
-    # apaga um cliente da lista da agencia
-    def excluir_cliente(self, cliente):
-        if cliente in self._cliente:
-            self._cliente.pop(cliente)
-
-
-class ContaBancaria:
-    # Construtor da classe, inicia com o nome do titular e saldo (opcional, padrão 0)
-    def __init__(self, nome, saldo=0):
-        self.nome = nome 
-        self.saldo = saldo 
 
     # Método para depositar dinheiro na conta
     def depositar(self, valor):
@@ -78,7 +61,27 @@ class ContaBancaria:
             print(f"Saque de R${valor:.2f} realizado. Saldo atual: R${self.saldo:.2f}")
         else:
             print("Saldo insuficiente ou valor de saque inválido.")
-
-    # Método para consultar o saldo atual da conta
+    
+        # Método para consultar o saldo atual da conta
     def consultar_saldo(self):
         print(f"Saldo atual: R${self.saldo:.2f}")
+
+
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+class Agencia:
+    # cria a lista de cliente da agencia bancaria
+    def __init__(self):
+        self._cliente = []
+
+    # adiciona um novo cliente a lista criada
+    def novo_cliente(self, cliente):
+        self._cliente.append(cliente) 
+
+    # apaga um cliente da lista da agencia
+    def excluir_cliente(self, cliente):
+        if cliente in self._cliente:
+            self._cliente.pop(cliente)
